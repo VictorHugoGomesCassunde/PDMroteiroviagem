c7db9a60d72cd131bfedea54c9f1fdcf2addac9c
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-11-25 22:38:58 -0400

    tentativa

--- workbox-config.js
+++ workbox-config.js
@@ -3,36 +3,57 @@ module.exports = {
   globPatterns: [
     "**/*.{html,js,css,png,svg,jpg}"
   ],
+
   swDest: "dist/sw.js",
+
   runtimeCaching: [
     {
-      urlPattern: ({url}) => url.origin.includes("tile.openstreetmap.org"),
+      
+      urlPattern: ({ url }) =>
+        url.origin.includes("tile.openstreetmap.org"),
+
       handler: "CacheFirst",
+
       options: {
         cacheName: "map-tiles",
         expiration: {
           maxEntries: 200,
-          maxAgeSeconds: 60 * 60 * 24 * 7 // 7 dias
+          maxAgeSeconds: 60 * 60 * 24 * 7 
         }
       }
     },
+
     {
-      urlPattern: ({request}) => request.destination === "script",
+      
+      urlPattern: ({ request }) =>
+        request.destination === "script",
+
       handler: "StaleWhileRevalidate",
+
       options: {
         cacheName: "js-cache"
       }
     },
+
     {
-      urlPattern: ({request}) => request.destination === "style",
+   
+      urlPattern: ({ request }) =>
+        request.destination === "style",
+
       handler: "StaleWhileRevalidate",
+
       options: {
         cacheName: "css-cache"
       }
     },
+
     {
-      urlPattern: ({request}) => request.destination === "document",
+     
+      urlPattern: ({ request }) =>
+        request.destination === "document",
+
       handler: "NetworkFirst",
+
       options: {
         cacheName: "html-cache"
       }
